1847604d6ec9ed4890da6a3dd040d272d9724783
Rishabh Malhotra <rishabh@squareup.com>
2025-07-20 23:18:38 -0400

    jquery update

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,12 @@ module.exports = (env, argv) => {
   return {
     mode: isProduction ? 'production' : 'development',
     entry: {
-      main: './src/js/app.js',
-      vendor: ['three'],
+      main: './src/js/app.js'
     },
     output: {
+      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist/js'),
-      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
-      publicPath: '/js/',
+      clean: true
     },
     module: {
       rules: [
@@ -25,47 +24,11 @@ module.exports = (env, argv) => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-class-properties'],
-            },
-          },
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js'],
-      alias: {
-        '@': path.resolve(__dirname, 'src/js'),
-        '@modules': path.resolve(__dirname, 'src/js/modules'),
-        '@config': path.resolve(__dirname, 'config'),
-      },
-    },
-    optimization: {
-      minimize: isProduction,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: isProduction,
-            },
-          },
-        }),
-      ],
-      splitChunks: {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendor',
-            priority: 10,
-          },
-          common: {
-            minChunks: 2,
-            priority: 5,
-            reuseExistingChunk: true,
-          },
-        },
-      },
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
     },
     plugins: [
       new CleanWebpackPlugin(),
@@ -73,18 +36,24 @@ module.exports = (env, argv) => {
         patterns: [
           {
             from: 'node_modules/jquery/dist/jquery.min.js',
-            to: '../lib/jquery/jquery.min.js',
-          },
-        ],
-      }),
+            to: path.resolve(__dirname, 'lib/jquery/jquery.min.js')
+          }
+        ]
+      })
     ],
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
-    stats: {
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false,
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments: false
+            }
+          },
+          extractComments: false
+        })
+      ]
     },
+    devtool: isProduction ? 'source-map' : 'eval-source-map'
   };
 }; 
